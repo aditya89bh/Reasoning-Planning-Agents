114a3f3f937cbeb4ff1cfda7fecd4a80380c9fe9
Aditya bhatia <aditya89bh@gmail.com>
2026-05-13 15:16:39 +0530

    Add integrated demo to unified demo runner

--- run_all_demos.sh
+++ run_all_demos.sh
@@ -5,3 +5,4 @@ python projects/01_task_decomposition_agent/run_demo.py
 python projects/02_planner_executor_agent/run_demo.py
 python projects/03_reflection_agent/run_demo.py
 python projects/04_multi_agent_planning/run_demo.py
+python integrated_demo/run_integrated_demo.py
